2af4b68da6aa4196bd1b8466e6d9370f4e12f550
Simon Brown <sbrown@skidgets.com>
2011-01-25 18:48:45 -0500

    Hello Project 01

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
-?>
+/** 
+ * My First Doc
+ * Simon Brown
+ * sbrown@skwidgets.com
+ */
+
+
+ob_start();
+
+	echo "My First Project with Output Buffering";
+
+	$_v = ob_get_contents();
+
+	ob_end_clean();
+
+echo $_v;
+
