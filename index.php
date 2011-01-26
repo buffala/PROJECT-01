a4aab31377fb365ce56666c1414a6f922c22f1f7
Simon Brown <sbrown@skidgets.com>
2011-01-25 19:10:33 -0500

    Everything else added

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
 /** 
- * My First Doc
- * Simon Brown
- * sbrown@skwidgets.com
+ * 
+ * @My First Doc
+ * @Simon Brown
+ * @sbrown@skwidgets.com
+ * 
  */
-
 
 ob_start();
 
-	echo "My First Project with Output Buffering";
+echo "My First Project with Output Buffering";
 
-	$_v = ob_get_contents();
+$_v = ob_get_contents();
 
-	ob_end_clean();
+ob_end_clean();
 
 echo $_v;
 
