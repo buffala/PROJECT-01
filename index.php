9ded7da39d937eb25e5fc4eee07ee2dbcb8659a0
Simon Brown <sbrown@skwidgets.com>
2011-02-07 19:31:44 -0500

    added comment

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
 <?php
-/** 
+
+/**
  * 
- * @My First Doc
- * @Simon Brown
- * @sbrown@skwidgets.com
+ * Project-01
+ * Simon A Brown
+ * ANM 293 Advance PHP - Brendan Howard
+ * Tuesday 6pm - 9pm
  * 
  */
 
+/**
+ * 
+ * output buffering beginning at start of file to store rendered page data into a variable
+ *  until completely loaded before displaying page.
+ */
 ob_start();
 
+/**
+ * 
+ * content that will be stored for output
+ */
 echo "My First Project with Output Buffering";
 
-$_v = ob_get_contents();
 
+/**
+ * 
+ * variable to store buffered output data
+ * @var unknown_type
+ */
+$v = ob_get_contents();
+
+
+/**
+ * 
+ * clean out the buffer after variable is set with data
+ */
 ob_end_clean();
 
-echo $_v;
 
+/**
+ * 
+ * display page content from set variable
+ */
+echo $v;
